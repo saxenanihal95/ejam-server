c6c0a781f502611ae2e84c993c4debb513113cba
Nihal Saxena <saxenanihal95@gmail.com>
2020-05-09 14:26:26 +0530

    Added mongodb

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,22 @@
 import express = require("express");
+var mongoose = require("mongoose");
+require("dotenv").config();
 
 // Create a new express application instance
 const app: express.Application = express();
+
+const username = process.env.MONGO_DB_USER_NAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+const mLabUrl = `mongodb://${username}:${password}@ds357955.mlab.com:57955/ejam-demo`;
+
+mongoose.connect(mLabUrl, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("connected");
+});
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
